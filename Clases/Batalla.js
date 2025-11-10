c0c1bba7e1e450675640b75d6af7b54331806220
Aleh-18 <alejandrorastrollo2006@gmail.com>
2025-11-10 19:06:59 +0100

    Fase 3 V1.1
    Un pequeño detallito

--- Clases/Batalla.js
+++ Clases/Batalla.js
@@ -21,11 +21,13 @@ export class Batalla {
 
                 this.#guerreroA.atacar(this.#guerreroB);
                 if (this.#guerreroB.vida <= 0) {
+                    this.#guerreroB.fallecido = true;
                     this.#guerrerosFallecidos.push(this.#guerreroB);
                     guerrerosCasaB.splice(0, 1);
                 }
                 this.#guerreroB.atacar(this.#guerreroA);
                 if (this.#guerreroA.vida <= 0) {
+                    this.#guerreroA.fallecido = true;
                     this.#guerrerosFallecidos.push(this.#guerreroA);
                     guerrerosCasaA.splice(0, 1);
                 }
